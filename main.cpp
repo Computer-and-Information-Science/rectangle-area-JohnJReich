212acdfb7457c1851fb9bb4b5eb85f5ebcb7c54d
John Reichard <johnjreich@gmail.com>
2026-02-02 04:56:40 +0000

    program cool

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
-
-
+float area = length * width;
+float perimeter = 2 * length + 2 * width; 
+cout << "Rectengale Properties:\n";
+cout << "Length = " << length << endl;
+cout << "Width = " << width << endl;
+cout << "Perimeter = " << perimeter << endl;
+cout << "Area = " << area << perimeter << endl; 
 }
